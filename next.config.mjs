ba15695fd0375bf9cb23be0cd5e025c4b86230ad
kiraaziz <kiraaziz2020@gmail.com>
2025-04-15 16:57:17 +0200

    fix build

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const config = {
   reactStrictMode: true,
   images: {
@@ -12,6 +14,16 @@ const config = {
       },
     ],
   },
+  webpack(config, { isServer }) {
+    // Exclude ./docs folder from the build process
+    config.module.rules.push({
+      test: /docs/,
+      loader: 'ignore-loader', // Ignore loading files from ./docs folder
+      include: path.resolve(__dirname, 'docs'),
+    });
+
+    return config;
+  },
 };
 
-export default config
+export default config;
